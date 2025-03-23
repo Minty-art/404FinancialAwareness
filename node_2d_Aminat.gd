310fefaa4d94cbbc7fac01ec3f7faa19863986c2
Minty-art <70672363+Minty-art@users.noreply.github.com>
2025-03-23 10:11:33 -0500

    Add files via upload
    .tscn and .gd
    Uploaded

---
+++ node_2d_Aminat.gd
@@ -0,0 +1,196 @@
+extends Node2D
+
+
+
+
+
+
+
+var event = randi_range(0,5)
+
+# Practice Function
+func new_Day() -> void:
+	print(event)
+	match event:
+		0:
+			print("No Events Today!")
+		1:
+			print("It's Payday!")
+		
+		2:
+			print("Am I insured?")
+		 
+		3: 
+			print("Bills...")
+		4:
+			print("Credit Mail")
+		5:
+			print("Emergency")
+			
+
+
+
+
+
+
+
+
+#PAYDAY EVENT
+func _on_Bankbutton_pressed() -> void:
+			print("IT WORKED TO THE BANK");
+			$PaydayEvent.hide()
+			print("hide")
+			#$PaydayEvent.visible = false
+			print("Payday is set to false")
+			$BankChoice.show()
+
+
+func _on_InvestButton_pressed() -> void:
+			print("IT WORKED INVEST");
+			$PaydayEvent.hide()
+			print("hide")
+			$InvestChoice.show();
+			#$PaydayEvent.visble = false
+			print("Payday is set to false")
+			
+
+
+func _on_MattressButton_pressed() -> void:
+			print("IT WORKED MATTRESS");
+			$PaydayEvent.hide()
+			print("hide")
+			#$PaydayEvent.visible = false
+			print("Payday is set to false")
+			$MattressChoice.show()
+			
+
+
+func _on_exit_bank_pressed() -> void:
+			print ("Exit Done")
+			$BankChoice.hide()
+			$BankChoice.visible = false
+			print("No Render Done")
+
+
+func _on_exit_invest_pressed() -> void:
+			print ("Exit Done")
+			$InvestChoice.hide()
+			$InvestChoice.visible = false
+			print("No Render Done")
+
+
+func _on_exit_mattress_pressed() -> void:
+			print ("Exit Done")
+			$MattressChoice.hide()
+			$MattressChoice.visible = false
+			print("No Render Done")
+
+
+
+#
+#
+#
+#
+#
+# LIFE INSURANCE EVENT
+func _on_term_pressed() -> void:
+	print("IT WORKED GET THAT TERM");
+	$LifeInsuranceEvent.hide()
+	print("hide")
+	$TermChoice.show();
+	#$PaydayEvent.visble = false
+	print("Payday is set to false")
+
+	
+
+
+func _on_universal_life_pressed() -> void:
+	print("IT WORKED GET THAT UL");
+	$LifeInsuranceEvent.hide()
+	print("hide")
+	$UL_Choice.show();
+	#$PaydayEvent.visble = false
+	print("Payday is set to false")
+
+
+func _on_sliced_bread_pressed() -> void:
+	print("IT WORKED GET THAT SLICED BREAD");
+	$LifeInsuranceEvent.hide()
+	print("hide")
+	$IUL_Choice.show();
+	#$PaydayEvent.visble = false
+	print("Payday is set to false")
+
+
+func _on_exit_term_pressed() -> void:
+	print ("Exit Done")
+	$TermChoice.hide()
+	$TermChoice.visible = false
+	print("No Render Done")
+
+
+func _on_exit_ul_pressed() -> void:
+	print ("Exit Done")
+	$UL_Choice.hide()
+	$UL_Choice.visible = false
+	print("No Render Done")
+
+
+func _on_exit_iul_pressed() -> void:
+	print ("Exit Done")
+	$IUL_Choice.hide()
+	$IUL_Choice.visible = false
+	print("No Render Done")
+
+
+
+
+
+
+
+
+#/ CREDIT CARD EVENT
+
+func _on_yes_pressed() -> void:
+	print("IT WORKED GET THAT....credit card");
+	$CreditCardEvent.hide()
+	print("hide")
+	$new_CardChoice.show();
+	#$PaydayEvent.visble = false
+	print("Payday is set to false")
+
+
+
+func _on_no_pressed() -> void:
+	print("IT WORKED GET THAT....credit card");
+	$CreditCardEvent.hide()
+	print("hide")
+	$no_CardChoice.show();
+	#$PaydayEvent.visble = false
+	print("Payday is set to false")
+
+
+func _on_exit_new_card_pressed() -> void:
+	print("IT WORKED GET THAT....credit card");
+	$new_CardChoice.hide()
+	print("hide")
+	$extraCardFact.show();
+	#$PaydayEvent.visble = false
+	print("Payday is set to false")
+
+
+func _on_exit_no_card_pressed() -> void:
+	print("IT WORKED GET THAT....credit card");
+	$no_CardChoice.hide()
+	print("hide")
+	$extraCardFact.show();
+	#$PaydayEvent.visble = false
+	print("Payday is set to false")
+
+
+func _on_exit_extra_card_pressed() -> void:
+	print("IT WORKED GET THAT....credit card");
+	$extraCardFact.hide()
+	print("hide")
+	#$PaydayEvent.visble = false
+	print("Payday is set to false")
